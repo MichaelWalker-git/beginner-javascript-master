765904c5ef41472ae0a778abd546bc98310cd6f6
Dan Bantay <dbantayjr@gmail.com>
2020-01-04 14:04:17 -0800

    Udemy Net Ninja JS course practice

---
+++ exercises/udemy.js
@@ -0,0 +1,20 @@
+// strings
+console.log('it works!');
+
+const email = 'dbantayjr@gmail.com';
+console.log(email);
+
+// string concatenation
+const firstName = 'Dan';
+const lastName = 'Bantay';
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// sting methods
