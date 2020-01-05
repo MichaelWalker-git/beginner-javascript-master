01d2dea18c5ba26aaffc6de75f298b3b45d61866
Dan Bantay <dbantayjr@gmail.com>
2020-01-04 16:10:25 -0800

    numbers module

--- exercises/udemy.js
+++ exercises/udemy.js
@@ -1,20 +1,56 @@
-// strings
-console.log('it works!');
+// // strings
+// console.log('it works!');
 
-const email = 'dbantayjr@gmail.com';
-console.log(email);
+// const email = 'dbantayjr@gmail.com';
+// console.log(email);
 
-// string concatenation
-const firstName = 'Dan';
-const lastName = 'Bantay';
+// // string concatenation
+// const firstName = 'Dan';
+// const lastName = 'Bantay';
 
-const fullName = `${firstName} ${lastName}`;
-console.log(fullName);
+// const fullName = `${firstName} ${lastName}`;
+// console.log(fullName);
 
-// getting characters
-console.log(fullName[2]);
+// // getting characters
+// console.log(fullName[2]);
 
-// string length
-console.log(fullName.length);
+// // string length
+// console.log(fullName.length);
 
-// sting methods
+// // sting methods
+// // add .() to methods
+// console.log(fullName.toLocaleUpperCase());
+// const result = fullName.toLocaleLowerCase();
+// console.log(result, fullName);
+
+// // indexOf is a method
+// // @ is an arguement
+// const index = email.indexOf('@');
+// console.log(index);
+
+// common string methods
+
+// const email = 'dbantayjr@gmail.com';
+
+// // const result = email.lastIndexOf('l');
+
+// // const result = email.slice(2, 5);
+
+// // let result = email.replace('n', 'r');
+
+
+
+// console.log(result);
+
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+
+let result = pi * radius**2
+console.log(result);
