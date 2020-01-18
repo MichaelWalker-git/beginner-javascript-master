b0a6d32eaca49bf840d1ad908492a4b17b4ab592
Dan Bantay <dbantayjr@gmail.com>
2020-01-18 13:02:32 -0800

    Scroll to accept module finished

--- exercises/Scroll-to-accept/scroll-to-accept.js
+++ exercises/Scroll-to-accept/scroll-to-accept.js
@@ -1,1 +1,26 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+// Payload is the data that is carried on behalf of an application
+function obCallback(payload) {
+  //   removes the disbaled attribute on the button
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+// IntersectionObserver will watch if an element is on or off the page or partly on or off the page
+// pass in a callback (a function that will get called back at a certain point)
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+  // this tells me when im 100% on the page
+});
+
+// lastElementChild is way to get the last Element in the terms and conditions div
+ob.observe(terms.lastElementChild);
+
+// take the observer then call the observe method then pass it something to watch for. ex. (watch)
